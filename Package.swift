26456c8fb4a3b6279afe9773791892bbd845d926
Kirill Selivanov <sks97@yandex.ru>
2023-01-20 10:51:12 +0300

    Upgraded version package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "CArchSwinject",
                       // platforms
-                      platforms: [.iOS(.v11)],
+                      platforms: [.iOS(.v13)],
                       // Products
                       products: [.library(name: "CArchSwinject", targets: ["CArchSwinject"])],
                       // Dependencies
